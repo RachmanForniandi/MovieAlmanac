cfd8413498be302543d756975e0d49a01556d719
RachmanForniandi <rachmanforniandi@gmail.com>
2022-04-17 06:51:05 +0700

    named id included view. Added injection & coroutine libraries.

---
+++ app/src/main/java/com/example/moviealmanac/repositories/MovieRepository.kt
@@ -0,0 +1,9 @@
+package com.example.moviealmanac.repositories
+
+import com.example.moviealmanac.networks.NetworkConfig
+
+interface MovieRepository {
+    //private var netConfig = NetworkConfig.useServiceApi()
+    //private var compositeDisposable = CompositeDisposable()
+
+}
---
+++ app/src/main/java/com/example/moviealmanac/repositories/TvShowRepository.kt
@@ -0,0 +1,4 @@
+package com.example.moviealmanac.repositories
+
+interface TvShowRepository {
+}
